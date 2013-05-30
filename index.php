0bf7fe6017d15959902e43a528ce3e5113f92632
Asad Rahman <asad.rahman@outlook.com>
2013-05-31 01:01:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once 'lib/DbSync.class.php';
 $user = 'root';
 $pass = 'yourpasswordhere';
 $host = 'localhost';
-$sourceDbName      = 'piiqa_invesp';
-$destinationDbName = 'pii_invesp';
+$sourceDbName      = 'source_db_name';
+$destinationDbName = 'destination_db_name';
 
 $sourceDb      = new DbConf($user, $pass, $host, $sourceDbName);
 $destinationDb = new DbConf($user, $pass, $host, $destinationDbName);
